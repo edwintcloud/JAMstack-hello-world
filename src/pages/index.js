13b189e0b16c86676fcbb57090a29e853ac48df5
Edwin Cloud <ecloud412@gmail.com>
2018-09-05 09:00:28 -0700

    First Commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>My name is Edwin and I am a mostly self-taught and highly passionate Software Developer.</p>
+    <p>I am currently attending Make School.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
